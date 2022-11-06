c376ad78798db0ad9b4c2005e45f0a0a9742a6f9
nguyenhoaibao1 <nbao1467@gmail.com>
2022-11-06 22:01:11 +0700

    update button login

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
 import { Fragment } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { publicRoutes } from "./routes";
-import DefaultLayout from "./layouts/DefaultLayout";
+// import { DefaultLayout } from "./layouts";
 import Login from "./pages/Login"
 
 function App() {
   return (
-    // <Login></Login>
-    <div className="App">
-      <Routes>
-        {publicRoutes.map((route, index) => {
-          let Layout = DefaultLayout;
-          if (route.layout) {
-            Layout = route.layout;
-          } else if (route.layout === null) {
-            Layout = Fragment;
-          }
-          const Page = route.component;
-          return (
-            <Route
-              key={index}
-              path={route.path}
-              element={
-                <Layout>
-                  <Page />
-                </Layout>
-              }
-            />
-          );
-        })}
-      </Routes>
-    </div>
+    <Login></Login>
+    // <Router>
+    //   <div className="App">
+    //     <Routes>
+    //       {publicRoutes.map((route, index) => {
+    //         let Layout = DefaultLayout;
+    //         if (route.layout) {
+    //           Lgit ayout = route.layout;
+    //         } else if (route.layout === null) {
+    //           Layout = Fragment;
+    //         }
+    //         const Page = route.component;
+    //         return (
+    //           <Route
+    //             key={index}
+    //             path={route.path}
+    //             element={
+    //               <Layout>
+    //                 <Page />
+    //               </Layout>
+    //             }
+    //           />
+    //         );
+    //       })}
+    //     </Routes>
+    //   </div>
+    // </Router>
   );
 }
 
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -2,6 +2,7 @@ import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import logo from "./logo.jpg";
 import gg_icon from "../../assets/images/google-icon.jpg";
+import user_icon from "../../assets/images/user-icon.png";
 
 import { GoogleLogin } from 'react-google-login';
 
@@ -24,13 +25,13 @@ function Login() {
                     <div className={cx("logo")}>
                         <img src={logo}></img>
                     </div>
-                    <div className={cx("box-name")}> <h2>Đăng nhập vào F8</h2></div>
+                    <div className={cx("box-name")}> <h2>Đăng nhập</h2></div>
                     <div className={cx("login-options-box")}>
                         
                         <div className={cx("login-option")}>
                             <button >
                                 <div className={cx("icon")}>
-                                    <img src={gg_icon}></img>
+                                    <img src={user_icon}></img>
                                 </div>
                                 <span>Đăng nhập bằng email / số điện thoại</span>
                             </button>
@@ -44,19 +45,8 @@ function Login() {
                                 <span>Đăng nhập bằng Google</span>
                             </button>
                         </div>
-
-                        <div className={cx("login-option")}>
-                            <button >
-                                <div className={cx("icon")}>
-                                    <img src={gg_icon}></img>
-                                </div>
-                                <span>Đăng nhập bằng Github</span>
-                            </button>
-                        </div>
                     </div>
-
                     <p>Bạn chưa có tài khoản? <a href="#">Đăng ký</a></p>
-
                     <div className={cx("term")}>
                         <p> Việc bạn tiếp tục sử dụng trang web này đồng nghĩa bạn đồng ý với</p> <p><a href=""><u> Điều khoản sử dụng</u></a> của chúng tôi.</p>
                     </div>
